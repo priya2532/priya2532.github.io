4fd0f79b5251f2f97376f8ad6aeed27168e6f6ee
priya2532 <23r11a66b3@gcet.edu.in>
2025-05-17 18:13:04 +0530

    updated

---
+++ a12/index.js
@@ -0,0 +1,9 @@
+const product = [
+    {id:1,name:"apple",price:23},
+     {id:2,name:"orange",price:13},
+      {id:3,name:"banana",price:29}
+
+];
+
+product.forEach((product)=>console.log(product.name))
+product.forEach((product)=>console.log(product.price))
